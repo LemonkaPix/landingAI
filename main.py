f3de2aff50358343caf353ff5e4dad664d78b492
LemonkaPix <maciejwrobel029@gmail.com>
2025-05-25 16:46:23 +0200

    Network

--- main.py
+++ main.py
@@ -16,7 +16,17 @@ else:
     print('Selected device: CPU')
 
 
+class DQNetwork(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(DQNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_size)
 
+    def forward(self, state):
+        x = torch.nn.functional.leaky_relu(self.fc1(state), negative_slope=0.01)
+        x = torch.nn.functional.leaky_relu(self.fc2(x), negative_slope=0.01)
+        return self.fc3(x)
 
 
 env = gym.make("LunarLander-v3", render_mode="human")
