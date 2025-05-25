c9fe7d093a360dbb48b9e04e105711ce0cc0d385
LemonkaPix <maciejwrobel029@gmail.com>
2025-05-25 16:39:39 +0200

    optimalization

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import gymnasium as gym
+import torch
+from torch import nn
+
+
+OPTIMIZE_WITH_HARDWARE = False
+device = torch.device('cpu')
+if OPTIMIZE_WITH_HARDWARE:
+    if torch.backends.mps.is_available():
+        device = torch.device('mps')
+        print('Selected device: MPS (Metal Performace Shaders)')
+    elif torch.backends.cuda.is_flash_attention_available():
+        device = torch.device('cuda')
+        print('Selected device: GPU with CUDA support')
+else:
+    print('Selected device: CPU')
+
+
+
+
 
 env = gym.make("LunarLander-v3", render_mode="human")
 
